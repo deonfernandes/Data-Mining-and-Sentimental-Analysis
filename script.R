b689433e578d5c7c0cb5e937ad25491df2a1fb7e
Deon <109554722+deonfernandes@users.noreply.github.com>
2024-04-30 22:28:29 +0100

    Update script.R

--- script.R
+++ script.R
@@ -8,52 +8,43 @@ require_packages(c(
   "textdata"
 ))
 
-
-
-
-# Step 1: Fetch the RSS Feed using GET
+# get the RSS Feed using GET request
 response <- GET("http://rss.cnn.com/rss/cnn_latest.rss")
 content <- content(response, "text")
 
-# Step 2: Parse the RSS Feed
+#Parse the RSS Feed
 rss_feed <- read_xml(content)
 
-# Step 3: Extract news titles and descriptions
+# Extract news titles and descriptions from the RSS feed
 news_items <- xml_find_all(rss_feed, ".//item")
 titles <- xml_text(xml_find_all(news_items, ".//title"))
 descriptions <- xml_text(xml_find_all(news_items, ".//description"))
 
-# Include an id for each news item
+# Create a dataframe of item, title, and description
 news_data <- data.frame(id = seq_along(titles), title = titles, description = descriptions)
 
-# Tokenize descriptions into words, retaining the id for each news item
+# Tokenize descriptions into words
 words <- news_data %>%
-  mutate(text = paste(title, description)) %>%  # Combine title and description for comprehensive sentiment analysis
+  mutate(text = paste(title, description)) %>%  # Combine title and description 
   unnest_tokens(word, text) %>%
-  select(word)  # Keep the id alongside the word
+  select(word)
 
-
-
-
-# Step 5: Perform sentiment analysis using NRC lexicon
+#to perform sentiment analysis using NRC lexicon
 nrc <- read_csv("nrc.csv")
 
-
-# Join words with NRC lexicon and keep word data in the output
-emotion_words <- words %>%
+#to join words with NRC lexicon and keep word along with the count of sentiments
+sentiment_words <- words %>%
   inner_join(nrc, by = "word", relationship = 'many-to-many') %>%
   group_by(word, sentiment) %>%
   summarise(count = n(), .groups = 'drop')  # Count occurrences of each sentiment for each word
+print(sentiment_words)
 
-print(emotion_words)
-#################################################
-#################################################
 # Total occurrences of each sentiment
-total_sentiments <- emotion_words %>%
+total_sentiments <- sentiment_words %>%
   group_by(sentiment) %>%
   summarise(total = sum(count), .groups = 'drop')
 
-# Assuming total_sentiments is already calculated and includes each sentiment's total count
+#Total count of sentiments
 total_counts <- sum(total_sentiments$total)
 
 # Calculate the proportion of each sentiment and convert it to a percentage format
@@ -62,8 +53,7 @@ sentiment_proportions <- total_sentiments %>%
   mutate(proportion = sprintf("%.2f%%", proportion))  # Format the proportion as a percentage string with two decimal places
 
 # Print the sentiment proportions in percentage
-print(sentiment_proportions)
-
+print(sentiment_proportions) 
 
 sentiment_proportions |> write_csv(format(Sys.time(), "%Y-%m-%d-%H-%M.csv"))
 
